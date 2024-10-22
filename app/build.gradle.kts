4eb8ef207c24bc76408e2c10af5c93e9aeb8f5d8
AvidCoder27 <gilireeves@gmail.com>
2024-10-22 16:32:20 -0400

    update version to 0.1.1, code 5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.reeves.unitconverter"
         minSdk = 24
         targetSdk = 34
-        versionCode = 5
-        versionName = "0.1.0"
+        versionCode = 6
+        versionName = "0.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
